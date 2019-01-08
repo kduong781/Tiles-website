7c24ec024b01ecfdfeedd986f900c5e8d60a6c87
kev <kduong5@yahoo.com>
2019-01-07 19:45:53 -0800

    fix product

--- app/controllers/productController.js
+++ app/controllers/productController.js
@@ -14,52 +14,44 @@ app.controller('myCtrl', function($scope) {
       this.desc = desc;
     }
 
-    var t1 = [
-      new Product("t1p1","./assets/images/gal-8.jpeg","type1 product1"),
-      new Product("t1p2","./assets/images/gal-9.jpeg","type1 product2"),
-      new Product("t1p3","./assets/images/gal-10.jpeg","type1 product3"),
+    var floor = [
+      new Product("KT-01","./assets/images/kt/tile-1.jpg","type1 product1"),
+      new Product("KT-02","./assets/images/kt/tile-2.jpg","type1 product2"),
+      new Product("KT-03","./assets/images/kt/tile-3.jpg","type1 product3"),
+      new Product("KT-04","./assets/images/kt/tile-4.jpg","type1 product3"),
+      new Product("KT-05","./assets/images/kt/tile-5.jpg","type1 product3"),
     ];
-    var t2 = [
-      new Product("t2p1","./assets/images/gal-11.jpeg","type2 product1"),
-      new Product("t2p2","./assets/images/gal-12.jpeg","type2 product2"),
-      new Product("t2p3","./assets/images/gal-13.jpeg","type2 product3"),
+    var kitchen = [
+      new Product("KT-01","./assets/images/kt/tile-6.jpg","type1 product1"),
+      new Product("KT-02","./assets/images/kt/tile-7.jpg","type1 product2"),
+      new Product("KT-03","./assets/images/kt/tile-8.jpg","type1 product3"),
+      new Product("KT-04","./assets/images/kt/tile-9.jpg","type1 product3"),
+      new Product("KT-05","./assets/images/kt/tile-10.jpg","type1 product3"),
     ]
-    var t3 = [
-      new Product("t3p1","./assets/images/gal-14.jpeg","type3 product1"),
-      new Product("t3p2","./assets/images/gal-10.jpeg","type3 product2"),
-      new Product("t3p3","./assets/images/gal-9.jpeg","type3 product3"),
+    var wall = [
+      new Product("KT-01","./assets/images/kt/tile-11.jpg","type1 product1"),
+      new Product("KT-02","./assets/images/kt/tile-9.jpg","type1 product2"),
+      new Product("KT-03","./assets/images/kt/tile-13.jpg","type1 product3"),
+      new Product("KT-04","./assets/images/kt/tile-14.jpg","type1 product3"),
+      new Product("KT-05","./assets/images/kt/tile-15.jpg","type1 product3"),
     ]
-    var t4 = [
-      new Product("t4p1","./assets/images/gal-7.jpeg","type4 product1"),
-      new Product("t4p2","./assets/images/gal-9.jpeg","type4 product2"),
-      new Product("t4p3","./assets/images/gal-4.jpeg","type4 product3"),
-    ]
-    var t5 = [
-      new Product("t5p1","./assets/images/gal-6.jpeg","type5 product1"),
-      new Product("t5p2","./assets/images/gal-9.jpeg","type5 product2"),
-      new Product("t5p3","./assets/images/gal-13.jpeg","type5 product3"),
-    ]
-    var t6 = [
-      new Product("t6p1","./assets/images/gal-12.jpeg","type6 product1"),
-      new Product("t6p2","./assets/images/gal-9.jpeg","type6 product2"),
-      new Product("t6p3","./assets/images/gal-11.jpeg","type6 product3"),
-    ]
-    var t7 = [
-      new Product("t7p1","./assets/images/gal-4.jpeg","type7 product1"),
-      new Product("t7p2","./assets/images/gal-9.jpeg","type7 product2"),
-      new Product("t7p3","./assets/images/gal-14.jpeg","type7 product3"),
+    var bathroom = [
+      new Product("KT-01","./assets/images/kt/tile-16.jpg","type1 product1"),
+      new Product("KT-02","./assets/images/kt/tile-2.jpg","type1 product2"),
+      new Product("KT-03","./assets/images/kt/tile-13.jpg","type1 product3"),
+      new Product("KT-04","./assets/images/kt/tile-4.jpg","type1 product3"),
+      new Product("KT-05","./assets/images/kt/tile-15.jpg","type1 product3"),
     ]
 
     $scope.types =
     [
-      new Type("type1", "./assets/images/gal-1.jpeg",t1),
-      new Type("type2", "./assets/images/gal-2.jpeg",t2),
-      new Type("type3", "./assets/images/gal-3.jpeg",t3),
-      new Type("type4", "./assets/images/gal-1.jpeg",t4),
-      new Type("type5", "./assets/images/gal-5.jpeg",t5),
-      new Type("type6", "./assets/images/gal-6.jpeg",t6),
-      new Type("type7", "./assets/images/gal-7.jpeg",t7),
+      new Type("Floor", "./assets/images/gal-1.jpeg",floor),
+      new Type("Kitchen", "./assets/images/gal-2.jpeg",kitchen),
+      new Type("Wall", "./assets/images/gal-3.jpeg",wall),
+      new Type("Bathroom", "./assets/images/gal-1.jpeg",bathroom),
     ];
+
+    //Floor,Kitchen,Wall,Bathroom
 
     $scope.showProducts = function(name,products) {
       $scope.view = "Products";
