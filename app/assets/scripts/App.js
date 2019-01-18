2ae8cb9421a562551c990d64ca3ae0cd331fd915
Kevin <kduong781@gmail.com>
2019-01-17 19:16:27 -0800

    phase2

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -7,3 +7,22 @@ var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
 var stickyHeader = new StickyHeader();
+
+$(".gallery__item").on("click", function()
+{
+  var modal = document.getElementById('myModal');
+  // Get the image and insert it inside the modal - use its "alt" text as a caption
+  var modalImg = document.getElementById("img01");
+  var captionText = document.getElementById("caption");
+  modal.style.display = "block";
+  modalImg.src =$(this)[0].childNodes[0].src;
+  captionText.innerHTML = $(this)[0].childNodes[0].alt;
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+});
